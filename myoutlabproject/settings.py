49ab9ad058be5ae2ea34cbff9f4208a48ba36ba3
aka2910 <20005004@iitb.ac.in>
2021-09-09 09:51:12 +0530

    adding heroku

--- myoutlabproject/settings.py
+++ myoutlabproject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,4 +129,3 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
-django_heroku.settings(locals())
