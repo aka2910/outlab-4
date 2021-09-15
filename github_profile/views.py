e9811ea67b7f1ac3bd5848e279a7dd8e49c02088
aka2910 <200050004@iitb.ac.in>
2021-09-16 00:07:30 +0530

    bad api response resolved

--- github_profile/views.py
+++ github_profile/views.py
@@ -5,18 +5,30 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.utils.timezone import get_current_timezone
 from django.views import generic
-from django.contrib.messages.views import SuccessMessageMixin
-
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 from .fetch_git import *
 from .forms import *
 from .models import Profile1
 
 
-class SignUpView(SuccessMessageMixin, generic.CreateView):
+class SignUpView(generic.FormView):
     form_class = SignupForm
     template_name = 'registration/signup.html'
-    success_url = reverse_lazy('login')
-    success_message = f"User was created successfully"
+    success_url = ''
+    def form_valid(self, form):
+        form.save()
+        username = self.request.POST['username']
+        resp = get_github_users_response(username)
+        if resp is not None:
+            messages.success(self.request, f" {username} was created successfully! Login Now")
+            self.success_url = '../accounts/login'
+            return HttpResponseRedirect(self.success_url)
+        else:
+            self.success_url = '../signup'
+            messages.success(self.request, f" {username} could not be created due to bad response from github api")
+            return HttpResponseRedirect(self.success_url)
 
 
 def exploreView(request):
@@ -38,6 +50,7 @@ def updateView(request, value):
     if repo is not None:
         repo = filter_json(repo)
     profile.follower_count = resp['followers']
+    profile.avatar_url = resp['avatar_url']
     profile.repos = repo
     profile.last_update = datetime.now(tz=get_current_timezone())
     profile.save()
